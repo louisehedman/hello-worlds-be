05062c33883bc462332677a83764766e4f992a34
louisehedman <louise.hedman@chasacademy.se>
2022-05-14 11:59:22 +0200

    add routes

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 4000;
 connectDB()
 
 app.use(express.json());
-//app.use("/api/auth", require('./routes/auth'));
+app.use("/api/auth", require('./routes/auth'));
 //app.use("/api/private", require('./routes/private'));
 
 //app.use(errorHandler);
