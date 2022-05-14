bcf47fb5b2cd45d7aaadba1ffaadaa7f249fa145
louisehedman <louise.hedman@chasacademy.se>
2022-05-14 11:56:42 +0200

    add schema methods

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-import bcrypt from 'bcryptjs';
+import bycrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import crypto from 'crypto';
 import { model, Schema, Model, Document } from 'mongoose';
@@ -47,3 +47,35 @@ const UserSchema: Schema = new Schema({
         avatar: String,
     }
 });
+
+UserSchema.pre<IUser>("save", async function (next: any) {
+    if (!this.isModified('password')) {
+        return next();
+    }
+    const salt = await bycrypt.genSalt(10);
+    this.password = bycrypt.hashSync(this.password, 10);
+    next();
+});
+
+UserSchema.methods.matchPassword= async function (password:string) {
+    return await bycrypt.compare(password,this.password)   
+}
+
+UserSchema.methods.getSignedToken= function (password:string) {
+    return jwt.sign({id:this._id},process.env.JWT_SECRET!,{
+        expiresIn:process.env.JWT_EXPIRE
+    })   
+}
+
+UserSchema.methods.getResetPasswordToken= function () {
+    const resetToken= crypto.randomBytes(20).toString('hex');
+    this.resetPasswordToken= crypto.
+    createHash('sha256')
+    .update(resetToken)
+    .digest('hex');  
+    this.resetPasswordExpire = Date.now() + 10*(60*1000) 
+    return resetToken
+
+}
+
+export const User:Model<IUser> = model("User", UserSchema);
