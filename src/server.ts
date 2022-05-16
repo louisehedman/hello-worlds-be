235c83f1470746e19ab3b1b3ec610a9221883574
louisehedman <louise.hedman@chasacademy.se>
2022-05-16 16:28:20 +0200

    setup server

--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,20 @@
-require('dotenv').config({path:'./config.env'});
-
+import "dotenv/config";
+import router from "./routes/auth";
 import express from 'express';
-import cors from 'cors';
 import cookieParser from 'cookie-parser';
-import mongoose from 'mongoose';
-import { connectDB } from "./config/db"
 
 const app = express();
 const PORT = process.env.PORT || 4000;
-import './utils/errorResponse';
 
-connectDB()
 
 app.use(express.json());
 app.use(cookieParser());
-app.use("/api/auth", require('./routes/auth'));
+app.use("/", router);
 
-const server=app.listen(
+app.listen(
     PORT, () => {
         console.log(`Server is running on port ${PORT}`)
     }
 );
 
-process.on("unhandledRejection",(error,promise)=>{
-    console.log(`Logged Error: ${error}`);
-    server.close(()=>process.exit(1))
-
-})
 
