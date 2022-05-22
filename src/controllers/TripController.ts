be840cb2593a1c60595cd712d75ccc059de0a3a6
Sandra Wallén <75799823+sandra-wallen@users.noreply.github.com>
2022-05-22 22:44:37 +0200

    add create trip functionality in controller

---
+++ src/controllers/TripController.ts
@@ -0,0 +1,27 @@
+import { Response, Request } from 'express';
+import User from '../database/models/User';
+import { ErrorResponse } from '../utils/errorResponse';
+
+const createList = async (req: Request, res: Response, next: any) => {
+  const { destination, travTime } = req.body;
+  const { id } = req.params;
+
+  try {
+    const updateList = await User.findByIdAndUpdate(id, {
+      list: {destination, travTime}
+    }, { new: true });
+
+    if (updateList) {
+      res.status(200).json({
+        success: true,
+        updateList
+      })
+    } else {
+      res.status(404).json({ success: false, message: 'Could not update list' })
+    }
+  } catch (error) {
+    return next(new ErrorResponse(error.message, 500))
+  }
+}
+
+export { createList };
