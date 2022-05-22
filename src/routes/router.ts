56c8acd58de075b1441cf977e1d2b69d0e3a10c6
Sandra Wallén <75799823+sandra-wallen@users.noreply.github.com>
2022-05-22 22:47:19 +0200

    import createTrip and set up route

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -3,13 +3,13 @@ const router = Router();
 
 //import controllers
 import { register, login, logout } from '../controllers/AuthController';
-import { createList } from '../controllers/TripController';
+import { createTrip } from '../controllers/TripController';
 
 //routes
 router.post('/register', register );
 router.post('/login', login);
 router.post('/logout', logout);
-router.patch('/create-trip/:id', createList);
+router.patch('/create-trip/:id', createTrip);
 
 
 export default router;
