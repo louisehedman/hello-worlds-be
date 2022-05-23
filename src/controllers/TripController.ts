69a90b6bd00a5eb449a5ec28f206dca47a4f30a0
Sandra Wallén <75799823+sandra-wallen@users.noreply.github.com>
2022-05-23 10:57:13 +0200

    add spread operator to createTrip to include all existing trips, add getList

--- src/controllers/TripController.ts
+++ src/controllers/TripController.ts
@@ -2,13 +2,34 @@ import { Response, Request } from 'express';
 import User from '../database/models/User';
 import { ErrorResponse } from '../utils/errorResponse';
 
+const getList = async (req: Request, res: Response, next: any) => {
+  const { id } = req.params;
+
+  try {
+    const user = await User.findById(id);
+
+    if (user) {
+      res.status(200).json({
+        success: true,
+        list: user.list
+      })
+    } else {
+      res.status(404).json({ success: false, message: 'Could not get list' })
+    }
+  } catch (error) {
+    return next(new ErrorResponse(error.message, 500))
+  }
+}
+
 const createTrip = async (req: Request, res: Response, next: any) => {
   const { destination, travTime } = req.body;
   const { id } = req.params;
 
   try {
+    const user = await User.findById(id);
+
     const updateList = await User.findByIdAndUpdate(id, {
-      list: {destination, travTime}
+      list: [...user.list, {destination, travTime}]
     }, { new: true });
 
     if (updateList) {
@@ -24,4 +45,4 @@ const createTrip = async (req: Request, res: Response, next: any) => {
   }
 }
 
-export { createTrip };
+export { getList, createTrip };
