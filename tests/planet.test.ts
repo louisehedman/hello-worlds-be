b7cebe03b687978cd641f23fc73d8b2451192a42
louisehedman <louise.hedman@chasacademy.se>
2022-05-25 16:29:38 +0200

    create test for register user

--- tests/planet.test.ts
+++ tests/planet.test.ts
@@ -1,7 +1,7 @@
 import 'jest';
 import Planet from '../src/database/models/Planet';
 
-import * as dbHandler from './db';
+import * as dbHandler from './mockdb';
 
 beforeAll(async () => {
     await dbHandler.connect()
@@ -18,18 +18,21 @@ afterAll(async () => {
 describe('planet test', () => {
     it('can be created correctly', async () => {
         // expect that one assertion will be made
-        expect.assertions(1)
+        expect.assertions(2)
         // create new planet model instance
         const planet = new Planet()
         // set some test properties
-        planet.name = 'Test name'
+        planet.name = 'Tellus',
+        planet.avgTemp = 20,
         // save test planet to in-memory db
         await planet.save()
         // find inserted planet by name
-        const postInDb = await Planet.findOne({name: 'Test name'}).exec()
-        console.log('Planet document from memory-db', postInDb)
+        const planetInDb = await Planet.findOne({name: 'Tellus'}).exec()
+        console.log('Planet document from memory-db', planetInDb)
         // check that name is expected
-        expect(postInDb.name).toEqual('Test name')
+        expect(planetInDb.name).toEqual('Tellus')
+        // check that temp is expected
+        expect(planetInDb.avgTemp).toEqual(20)
     });
 });
 
---
+++ tests/user.test.ts
@@ -0,0 +1,45 @@
+import 'jest';
+import User from '../src/database/models/User';
+
+import * as dbHandler from './mockdb';
+
+beforeAll(async () => {
+    await dbHandler.connect()
+});
+
+afterEach(async () => {
+    await dbHandler.clearDatabase()
+});
+
+afterAll(async () => {
+    await dbHandler.closeDatabase()
+});
+
+describe('user test', () => {
+    it('can be created correctly', async () => {
+        // expect that four assertions will be made
+        expect.assertions(4)
+        // create new user model instance
+        const user = await User.create({
+            firstName: 'Harry',
+            username: 'harrypotter',
+            email: 'harry@potter.test',
+            password: '12345678',
+            isAdmin: false
+        })
+        // set some test properties
+        await user.save()
+        // find inserted user by username
+        const userInDb = await User.findOne({username: 'harrypotter'}).exec()
+
+        console.log('User document from memory-db', userInDb)
+        // check that first name is expected
+        expect(userInDb.firstName).toEqual('Harry')
+        // check that user is expected
+        expect(userInDb.username).toEqual('harrypotter')
+        // check that email is expected
+        expect(userInDb.email).toEqual('harry@potter.test')
+        // check that role is expected
+        expect(userInDb.isAdmin).toEqual(false)
+    });
+});
