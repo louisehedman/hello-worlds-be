05131496c8ebedb7c0e379787b1d20899e109f5d
Sandra Wallén <75799823+sandra-wallen@users.noreply.github.com>
2022-05-25 14:58:00 +0200

    add get trip endpoint to router, use authorization middleware on accurate endpoints

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -2,15 +2,16 @@ import { Router } from "express";
 const router = Router();
 
 //import controllers
-import { register, login, logout } from '../controllers/AuthController';
-import { getList, createTrip } from '../controllers/TripController';
+import { register, login, logout, authorization } from '../controllers/AuthController';
+import { getList, createTrip, getTrip } from '../controllers/TripController';
 
 //routes
-router.post('/register', register );
+router.post('/register', register);
 router.post('/login', login);
-router.post('/logout', logout);
-router.get('/get-list/:id', getList);
-router.patch('/create-trip/:id', createTrip);
+router.post('/logout', authorization, logout);
+router.get('/get-list/:id', authorization, getList);
+router.get('/get-trip/:userId/:tripId', authorization, getTrip);
+router.patch('/create-trip/:id', authorization, createTrip);
 
 
 export default router;
