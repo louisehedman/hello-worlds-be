b9515260bc51c97d326aaa1eb2353c22c3ca0abd
Sandra Wallén <75799823+sandra-wallen@users.noreply.github.com>
2022-05-26 10:15:40 +0200

    Set up UserController and get single user endpoint, add route in router

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,26 @@
+import { Response, Request } from "express";
+import User from "../database/models/User";
+
+const getUser = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  try {
+    const user = await User.findById(id);
+
+    if (user) {
+      return res.status(200).json({
+        success: true,
+        user,
+      });
+    } else {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+  } catch (error) {
+    return res.status(500).json({ success: false, error });
+  }
+};
+
+export { getUser };
