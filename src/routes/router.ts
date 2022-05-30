3b3705e1495599c902bc98ff85c0b493d80b4d14
Sandra Wallén <75799823+sandra-wallen@users.noreply.github.com>
2022-05-30 16:13:38 +0200

    Remove authorization middleware from all endpoints for testing purposes

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import cors, { CorsOptions } from "cors";
 
-import { register, login, logout, authorization } from '../controllers/AuthController';
+import { register, login, logout } from '../controllers/AuthController';
 import { getList, createTrip, getTrip, editTrip } from '../controllers/TripController';
 import { getAllPlanets, getPlanet } from '../controllers/PlanetController';
 import { getUser } from '../controllers/UserController';
@@ -27,9 +27,9 @@ router.get('/planets/:planet', getPlanet);
 // Protected routes
 router.post('/logout', logout);
 router.get('/user/:id', getUser);
-router.get('/get-list/:id', authorization, getList);
-router.get('/get-trip/:userId/:tripId', authorization, getTrip);
-router.patch('/create-trip/:id', authorization, createTrip);
-router.patch('/edit-trip/:userId/:tripId', authorization, editTrip);
+router.get('/get-list/:id', getList);
+router.get('/get-trip/:userId/:tripId', getTrip);
+router.patch('/create-trip/:id', createTrip);
+router.patch('/edit-trip/:userId/:tripId', editTrip);
 
 export default router;
