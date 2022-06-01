3751cf2682564923eef6d37f551218077178cb7f
Sandra Wallén <75799823+sandra-wallen@users.noreply.github.com>
2022-06-01 19:20:25 +0200

    Add authorization middleware to all protected routes

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import cors, { CorsOptions } from "cors";
 
-import { register, login, logout } from '../controllers/AuthController';
+import { register, login, logout, authorization } from '../controllers/AuthController';
 import { getList, createTrip, getTrip, editTrip } from '../controllers/TripController';
 import { getAllPlanets, getPlanet } from '../controllers/PlanetController';
 import { getUser } from '../controllers/UserController';
@@ -21,15 +21,15 @@ router.post('/register', register);
 router.post('/login', login);
 
 // Planet routes
-router.get('/planets', getAllPlanets);
-router.get('/planets/:planet', getPlanet);
+router.get('/planets', authorization, getAllPlanets);
+router.get('/planets/:planet', authorization, getPlanet);
 
 // Protected routes
-router.post('/logout', logout);
-router.get('/user/:id', getUser);
-router.get('/get-list/:id', getList);
-router.get('/get-trip/:userId/:tripId', getTrip);
-router.patch('/create-trip/:id', createTrip);
-router.patch('/edit-trip/:userId/:tripId', editTrip);
+router.post('/logout', authorization, logout);
+router.get('/user/:id', authorization, getUser);
+router.get('/get-list/:id', authorization, getList);
+router.get('/get-trip/:userId/:tripId', authorization, getTrip);
+router.patch('/create-trip/:id', authorization, createTrip);
+router.patch('/edit-trip/:userId/:tripId', authorization, editTrip);
 
 export default router;
