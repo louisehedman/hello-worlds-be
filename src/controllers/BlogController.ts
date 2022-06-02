2f9187d7825f8c6afd01a0ae6264e5728ea447c3
Simon Miller <mail@simonmiller.se>
2022-06-02 13:57:27 +0200

    fix getBlog to find specific post

--- src/controllers/BlogController.ts
+++ src/controllers/BlogController.ts
@@ -3,32 +3,27 @@ import { Request, Response } from "express";
 import mongoose from "mongoose";
 
 const getBlogs = (req: Request, res: Response) => {
-    Blog.find({}, function (err: Error, blogs) {
-      if (!blogs) {
-        return res.status(404).json("No blogs found");
-      }
-      if (err) {
-        return res.status(500).send(console.error());
-      }
-      return res.status(200).json({ blogs });
-    });
+  Blog.find({}, function (err: Error, blogs) {
+    if (!blogs) {
+      return res.status(404).json("No blogs found");
+    }
+    if (err) {
+      return res.status(500).send(console.error());
+    }
+    return res.status(200).json({ blogs });
+  });
 };
 
 const getBlog = (req: Request, res: Response) => {
-    Blog.findOne(
-      { name: req.params.blog },
-      function (err: Error, blog: mongoose.Document) {
-        if (!blog) {
-          return res.status(404).json("No blog");
-        }
-        if (err) {
-          return res.status(500).send(console.error());
-        }
-        return res.status(200).json({ blog });
-      }
-    );
-  };
-
-
+  Blog.findById(req.params.id, function (err: Error, blog: mongoose.Document) {
+    if (!blog) {
+      return res.status(404).json("No blog");
+    }
+    if (err) {
+      return res.status(500).send(console.error());
+    }
+    return res.status(200).json({ blog });
+  });
+};
 
 export { getBlogs, getBlog };
